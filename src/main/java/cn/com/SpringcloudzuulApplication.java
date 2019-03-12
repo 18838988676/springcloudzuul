5c197ba6f17cdcbaa68f2c62965eaa412723c274
cec <cec@cec-PC>
2019-03-12 16:38:14 +0800

    这是面向传统网关的配置方式

--- src/main/java/cn/com/SpringcloudzuulApplication.java
+++ src/main/java/cn/com/SpringcloudzuulApplication.java
@@ -2,10 +2,18 @@ package cn.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
+//启动zuul代理功能
+@EnableZuulProxy
 public class SpringcloudzuulApplication {
 
+	
+	//http://localhost/wmcuser/user/userinfo      会自动的切换2个user中的userinfo方法；
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudzuulApplication.class, args);
 	}
