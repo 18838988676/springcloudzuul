ec8750231e88b6c51b91affe99023631ade8e00a
cec <cec@cec-PC>
2019-03-12 17:22:37 +0800

    //http://localhost/p/product/addUser  同时打开2个产品 2个用户，产品间出现负载均衡 产品调用2个用户的addUser方法 也会出现负载均衡

--- src/main/java/cn/com/SpringcloudzuulApplication.java
+++ src/main/java/cn/com/SpringcloudzuulApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 @EnableZuulProxy
 public class SpringcloudzuulApplication {
 
-	
+	//http://localhost/p/product/addUser  同时打开2个产品 2个用户，产品间出现负载均衡  产品调用2个用户的addUser方法 也会出现负载均衡
 	//http://localhost/wmcuser/user/userinfo      会自动的切换2个user中的userinfo方法；
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudzuulApplication.class, args);
